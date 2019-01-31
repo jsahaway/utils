e907ec8a94ea712ce69ade81ef895657f4abef09
jsahaway <jsahaway@gmail.com>
2019-01-31 21:46:49 +0100

    sroll

---
+++ JS_DOM_getScrollDirection.js
@@ -0,0 +1,30 @@
+"use strict";
+(() => {
+  // INIT
+  let testScroll = document.createElement("div");
+  testScroll.style.height = "200vh";
+  document.body.append(testScroll);
+
+  // SCROLL OBJ
+  let windowScroll = {
+    lastScroll: 0,
+    scroll: false,
+    setScroll() {
+      let currentScroll = window.pageYOffset;
+      this.scroll = currentScroll < this.lastScroll ? false : true;
+      this.lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+    }
+  };
+
+  // WINDOW EVENT
+  window.addEventListener("scroll", () => {
+    window.requestAnimationFrame(() => {
+      windowScroll.setScroll();
+      if (windowScroll.scroll) {
+        console.log("scroll down");
+      } else {
+        console.log("scroll up");
+      }
+    });
+  });
+})();
