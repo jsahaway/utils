237b9ee7dbb288d122be57dc127cc63336752376
jsahaway <jsahaway@gmail.com>
2019-02-03 18:49:29 +0100

    various

--- JS_customEventListener.js
+++ JS_customEventListener.js
@@ -1,67 +1,49 @@
-let _dispatchHandlers = [];
+///////////////////////////////////////////
+// event emmitter with array //////////////
+
+let _arrHandlers = [];
+
 let on = function(eventName, handler) {
   switch (eventName) {
     case "updateModel":
-      return _dispatchHandlers.push(handler);
+      return _arrHandlers.push(handler);
     case "updateView":
-      return _dispatchHandlers.push(handler);
-  }
-};
-let dispatch = function(eventName) {
-  for (let i = 0; i < _dispatchHandlers.length; i++) {
-    _dispatchHandlers[i]();
+      return _arrHandlers.push(handler);
   }
 };
 
+let dispatch = function() {
+  for (let i = 0; i < _arrHandlers.length; i++) {
+    _arrHandlers[i](); //.handler(_arrHandlers[i].eventName);
+  }
+};
 
-module.exports.convertPath = (val) => { // val = valeur à remplacer
-	let dir = process.cwd();
-	dir += val;
-	if (process.platform === 'windows') {
-		dir = dir.replace(/\//g, '\\')
-	}
-	console.log('--->', dir)
-	return dir;
-}
+// listen
+on("updateModel", a => {
+  console.log("whit arr");
+});
 
+// trig
+dispatch();
 
-module.exports.geo = {
-	findCrossing: (a, b, c, d) => {
-		let x1 = a.x,
-			y1 = a.y,
-			x2 = b.x,
-			y2 = b.y,
-			x3 = c.x,
-			y3 = c.y,
-			x4 = d.x,
-			y4 = d.y;
+////////////////////////////////////////////
+// event emmitter with object //////////////
 
-		var x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-		var y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-		if (isNaN(x) || isNaN(y)) {
-			return false;
-		} else {
-			if (x1 >= x2) {
-				if (!(x2 <= x && x <= x1)) { return false; }
-			} else {
-				if (!(x1 <= x && x <= x2)) { return false; }
-			}
-			if (y1 >= y2) {
-				if (!(y2 <= y && y <= y1)) { return false; }
-			} else {
-				if (!(y1 <= y && y <= y2)) { return false; }
-			}
-			if (x3 >= x4) {
-				if (!(x4 <= x && x <= x3)) { return false; }
-			} else {
-				if (!(x3 <= x && x <= x4)) { return false; }
-			}
-			if (y3 >= y4) {
-				if (!(y4 <= y && y <= y3)) { return false; }
-			} else {
-				if (!(y3 <= y && y <= y4)) { return false; }
-			}
-		}
-		return { x: x, y: y };
-	}
-}
+let _objHandlers = {};
+
+let on2 = function(eventName, handler) {
+  return (_objHandlers[eventName] = handler);
+};
+
+let dispatch2 = function(eventName) {
+  if (_objHandlers.hasOwnProperty(eventName)) _objHandlers[eventName](eventName);
+  else return undefined;
+};
+
+// listen
+on2("updateModel2", a => {
+  console.log("with obj >>", a);
+});
+
+// trig
+dispatch2("updateModel2");
